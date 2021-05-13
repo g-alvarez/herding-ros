0f9877b5b5610c1ddd2395040c12ae7a8672d7ec
Gabriel Alvarez <gabrielaar11@gmail.com>
2021-05-13 23:04:44 +0200

    Add function comments

--- swarm.py
+++ swarm.py
@@ -25,7 +25,7 @@ ATTRACTION_A = 2.5
 REPULSION_B = 10
 
 def plot_positions(positions):
-  """Plot the positions of each robot.
+  """Plot the positions of each robot in a 200x200 figure.
   
   The x and y axis limits:
     - x [-100, 100]
@@ -44,7 +44,7 @@ def get_neighbors(i, adj_matrix):
   """Get the neighbors of robot i, using the adjancency matrix.
 
   If the position j is equal to 1 and it is not the current robot, 
-  then it is a neighbor 
+  then it is a neighbor.
   """
   neighbors = []
   j = 0
@@ -55,59 +55,72 @@ def get_neighbors(i, adj_matrix):
   return neighbors
 
 def convert_range(x):
-  """Convert x value from [0, 1] to [-100, 100]
+  """Convert x value from range [0, 1] to range [-100, 100].
   """
   old_range = OLD_MAX - OLD_MIN  
   new_range = MAX - MIN  
   return (((x - OLD_MIN) * new_range) / old_range) + MIN
 
 def phi(value, beta=PHI_BETA, c=PHI_C):
-  """Solve quadratic equation via the quadratic formula.
+  """The generalized function.
 
-  A quadratic equation has the following form:
-  ax**2 + bx + c = 0
+  The formula:
+    phi(||y||) = exp(-||y|| ^ beta / c)
 
-  beta >= 1, c > 0
+  Where:
+    - beta >= 1
+    - c > 0
   """
   return math.exp(-1 * (value ** beta) / c)
 
 def gamma(pos_i, pos_j, alpha=GAMMA_ALPHA):
-  """Solve quadratic equation via the quadratic formula.
+  """The gamma function.
 
-  A quadratic equation has the following form:
-  ax**2 + bx + c = 0
+  The formula:
+    gamma(i, j) = 1 / ||Xi - Xj|| ^ alpha
 
-  alpha > 0
+  Where:
+    - alpha > 0
+    - Xi, Xj are 2D vectors
   """
   return 1 / (np.linalg.norm(pos_i - pos_j) ** alpha)
 
 def attraction_function(vector, a=ATTRACTION_A):
-  """Solve quadratic equation via the quadratic formula.
+  """The attraction function.
 
-  A quadratic equation has the following form:
-  ax**2 + bx + c = 0
+  The formula:
+    Ga(||y||) = a * (1 - phi(||y||)) 
 
-  a > 0
+  Where:
+    - a > 0
+    - y is a 2D vector
+    - phi is the generalized function
   """
   return a * (1 - phi(np.linalg.norm(vector)))
 
 def repulsion_function(vector, b=REPULSION_B):
-  """Solve quadratic equation via the quadratic formula.
+  """The repulsion function.
 
-  A quadratic equation has the following form:
-  ax**2 + bx + c = 0
+  The formula:
+    Gr(||y||) = b * phi(||y||) 
 
-  b > 0
+  Where:
+    - b > 0
+    - y is a 2D vector
+    - phi is the generalized function
   """
   return b * phi(np.linalg.norm(vector))
 
 def interaction_function(vector):
-  """Solve quadratic equation via the quadratic formula.
+  """The interaction function.
 
-  A quadratic equation has the following form:
-  ax**2 + bx + c = 0
-
-  There always two solutions to a quadratic equation: x_1 & x_2.
+  The formula:
+    g(y) = y / ||y|| * [( Gr(||y||) - Ga(||y||) )]
+    
+  Where:
+    - y is a 2D vector
+    - Gr is the repulsion function
+    - Ga is the attraction function
   """
   return vector / np.linalg.norm(vector) * (repulsion_function(vector) - attraction_function(vector))
 
@@ -126,7 +139,7 @@ if __name__ == "__main__":
   print(adj_matrix)
   print()
 
-  # The position (x, y) coordinate of robot i in 0..n-1
+  # The position (x, y) coordinates of robot i for i in 0..n-1
   positions = convert_range(np.random.rand(n, 2))
   print("Initial positions:")
   print(positions)
