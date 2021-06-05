f3d35b03b9f47116d72219dca1ecdc83b751a143
Gabriel Alvarez <gabrielaar11@gmail.com>
2021-06-05 17:31:15 +0200

    Add circular trajectory

--- swarm.py
+++ swarm.py
@@ -13,31 +13,59 @@ import matplotlib
 
 matplotlib.use("TkAgg")
 
-DELTA_T = 0.3
+DELTA_T = 0.03
 
 # np.random.rand range
-OLD_MIN = 0
-OLD_MAX = 1
+OLD_MIN = 0.0
+OLD_MAX = 1.0
 
 # Plot figure (x,y) axis range
-MIN = -100
-MAX = 100
+MIN = -2.0
+MAX = 2.0
 
 # The generalized function parameters
-PHI_BETA = 2
-PHI_C = 1
+PHI_BETA = 4.0
+PHI_C = 0.02
 
 # The gamma function parameter
-GAMMA_ALPHA = 1
+GAMMA_ALPHA = 1.0
 
 # The attraction function parameter
-ATTRACTION_A = 4
+ATTRACTION_A = 4.0
 
 # The repulsion function parameter
-REPULSION_B = 0.2
+REPULSION_B = 0.4
+
+# The leader speed parameter
+LEADER_SPEED = 0.005
+
+# The leader node attraction function parameter
+LEADER_ATTRACTION_A = 20.0
+
+# The obstacle node repulsion function parameter
+OBSTACLE_REPULSION_B = 20.0
 
 # The range of visibility
-R = 40
+R = 100.0
+
+# The angular speed
+DELTA_ANG = 0.5
+
+# The radius of the circular trajectory
+RADIUS = 1.0
+
+sin = lambda degs: math.sin(math.radians(degs))
+cos = lambda degs: math.cos(math.radians(degs))
+
+@beartype
+def circular_path(x: float, y: float, delta_angle: float = DELTA_ANG, 
+                  radius: float = RADIUS) -> Iterator[tuple]:
+  """Generate coords of a circular path every delta angle degrees.
+  """
+  angle = 0
+  while True:
+    yield (x + radius * cos(angle), y + radius * sin(angle))
+    angle = (angle + delta_angle) % 360
 
 @beartype
 def plot_positions(n: int, positions: np.ndarray) -> None:
@@ -52,10 +80,15 @@ def plot_positions(n: int, positions: np.ndarray) -> None:
   plt.ylim(MIN, MAX)
   i = 0
   for pos in positions:
+    # Robots
     if i < n:
       plt.plot(pos[0], pos[1], "bo")
-    else:
+    # Leader
+    if i == n:
       plt.plot(pos[0], pos[1], "ko")
+    # Obstacles
+    elif i > n:
+      plt.plot(pos[0], pos[1], "go")
     i += 1
   plt.draw()
   plt.show(block=False)
@@ -154,7 +187,7 @@ def repulsion_function(vector: np.ndarray, b: float = REPULSION_B) -> float:
   return b * phi(np.linalg.norm(vector))
 
 @beartype
-def interaction_function(vector: np.ndarray) -> np.ndarray:
+def interaction_function(vector: np.ndarray, neighbor: int, n: int) -> np.ndarray:
   """The interaction function.
 
   The formula:
@@ -165,24 +198,40 @@ def interaction_function(vector: np.ndarray) -> np.ndarray:
     - Gr is the repulsion function
     - Ga is the attraction function
   """
+  # Neighbor is leader node
+  if neighbor == n:
+    return vector / np.linalg.norm(vector) * (repulsion_function(vector) - attraction_function(vector, a=LEADER_ATTRACTION_A))
+  elif neighbor > n:
+    return vector / np.linalg.norm(vector) * (repulsion_function(vector, b=OBSTACLE_REPULSION_B) - attraction_function(vector))
   return vector / np.linalg.norm(vector) * (repulsion_function(vector) - attraction_function(vector))
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Swarm algorithm test.')
-  parser.add_argument('-n', '--robots', type=int, default=2, choices=range(2, 101), help='the number of robots')
-  parser.add_argument('-r', '--rate', type=int, default=5, choices=range(1, 11), help='the rate of plots')
-  parser.add_argument('-v', '--verbose', action="store_true", help='shows program state step by step')
+  parser.add_argument('-n', '--robots', type=int, default=2, choices=range(2, 101), 
+                      help='the number of robots')
+  parser.add_argument('-r', '--rate', type=int, default=5, choices=range(1, 11), 
+                      help='the rate of plots')
+  parser.add_argument('-v', '--verbose', action="store_true",
+                      help='shows program state step by step')
+  parser.add_argument('-m', '--motion', choices=["circ", "rect"], default="rect",
+                      help='leader node motion type')
 
   args = parser.parse_args()
   n = args.robots
-  
+  motion = args.motion
+
   # The position (x, y) coordinates of robot i for i in 0..n-1
-  positions = convert_range(np.random.rand(n, 2))
+  positions = convert_range(np.random.rand(n+1, 2))
   positions_next = np.zeros((n, 2))
+  positions[n] = np.array([0.0, 0.0])
+
+  if motion == "circ":
+    c = circular_path(positions[n][0], positions[n][1])
+    positions[n] = next(c)
 
   # The obstacle coordinates, a horizontal line y = 10.0
-  xaxis = np.linspace(0, 20, MAX)
-  yaxis = np.full(len(xaxis), 10.0, dtype=float)
+  xaxis = np.linspace(-1.0, 1.0, 4)
+  yaxis = np.full(len(xaxis), 1.0, dtype=float)
   obstacles = np.array(list(zip(xaxis,yaxis)))
 
   positions = np.concatenate((positions, obstacles), axis=0)
@@ -191,6 +240,7 @@ if __name__ == "__main__":
   adj_matrix = np.ones((len(positions), len(positions)), dtype=int)
   np.fill_diagonal(adj_matrix, 0)
 
+  k = 0
   while True:
     # Plot the positions of all robots
     plot_positions(n, positions)
@@ -205,26 +255,29 @@ if __name__ == "__main__":
         print("Robot [{}] - {}".format(i, positions[i]))
 
       sum_gamma_interaction_function = np.zeros(2)
-      sum_gamma = np.zeros(2)
+      sum_gamma = 0.0
       # Get the neighbors of robot i
       for j in get_neighbors(i, adj_matrix):
         gamma_res = gamma(positions[i], positions[j])
         sum_gamma_interaction_function += gamma_res * \
-            interaction_function(positions[i] - positions[j])
+            interaction_function(positions[i] - positions[j], j, n)
         sum_gamma += gamma_res
 
       # Update the position of robot i with the accumulated values
       if np.linalg.norm(sum_gamma) != 0:
         positions_next[i] += sum_gamma_interaction_function / sum_gamma * DELTA_T
+
+    for i in range(n):
+      positions[i] = positions_next[i]
       
-      # Make sure coordinates are inside the map
-      positions_next[i][0] = positions_next[i][0] if positions_next[i][0] >= MIN else MIN - positions_next[i][0] % MIN
-      positions_next[i][0] = positions_next[i][0] if positions_next[i][0] <= MAX else MAX - positions_next[i][0] % MAX
-      positions_next[i][1] = positions_next[i][1] if positions_next[i][1] <= MAX else MAX - positions_next[i][1] % MAX
-      positions_next[i][1] = positions_next[i][1] if positions_next[i][1] >= MIN else MIN - positions_next[i][1] % MIN
+    # Start moving the leader after 10 iterations
+    if k > 10:
+      if motion == "circ":
+        positions[n] = next(c)
+      else:
+        positions[n] += LEADER_SPEED
 
-    positions = positions_next
-
+    k += 1
     if args.verbose:
       print("Sleeping for {} seconds...\n".format(1 / args.rate))
     time.sleep(1 / args.rate)
